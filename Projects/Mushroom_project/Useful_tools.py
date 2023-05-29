7624527122c2d61301d26010c8190fa221513de1
Sufyan Saleem <s.f.saleem2@gmail.com>
2023-05-29 18:59:22 +0100

    modified:   Projects/Mushroom_project/Mushroom_Classifier.ipynb 	modified:   Projects/Mushroom_project/Useful_tools.py 	modified:   Projects/Mushroom_project/__pycache__/Useful_tools.cpython-39.pyc

--- Projects/Mushroom_project/Useful_tools.py
+++ Projects/Mushroom_project/Useful_tools.py
@@ -2,3 +2,13 @@ import numpy as np
 def accuracy_score(Y_pred, Y_true):
     acc = np.sum(np.equal(Y_true.flatten(), Y_pred)) / len(Y_true)
     return acc
+
+def labelencoder(data):
+    col = df.columns[0]
+    dict = {}
+    list = [x for x in data.unique()]
+    for enum in range(len(list)):
+        dict[list[enum]] = enum
+    for label in list:
+        data = data.apply(lambda x: dict[label] if x == label else x)
+    return data
