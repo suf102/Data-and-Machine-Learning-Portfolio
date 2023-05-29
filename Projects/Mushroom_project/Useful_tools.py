317aa6324a5590013c82227f4dd5c68453a12754
Sufyan Saleem <s.f.saleem2@gmail.com>
2023-05-29 19:01:28 +0100

    modified:   Projects/Mushroom_project/Mushroom_Classifier.ipynb 	modified:   Projects/Mushroom_project/Useful_tools.py

--- Projects/Mushroom_project/Useful_tools.py
+++ Projects/Mushroom_project/Useful_tools.py
@@ -1,7 +1,14 @@
+# To replace some of the more useful SKlearn functions I have written my own versions here
+
 import numpy as np 
+
+# This function will work out the accuracy as a percentage of the predictions of a model. 
+
 def accuracy_score(Y_pred, Y_true):
     acc = np.sum(np.equal(Y_true.flatten(), Y_pred)) / len(Y_true)
     return acc
+
+# This function works as a label encoder, taking categorical data and converting it to an enumerated dataset, useful for classification
 
 def labelencoder(data):
     col = df.columns[0]
